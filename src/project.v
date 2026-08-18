abe13b626e8382a1038237d2bfccd6483146369e
AdityaRPrasad <viparp15@gmail.com>
2026-08-18 13:53:31 +0530

    Refactor module definition and add default_nettype
    Removed copyright comment and added default_nettype directive.

--- src/project.v
+++ src/project.v
@@ -1,23 +1,31 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
 `timescale 1ns/1ps
-
 `default_nettype none
 
 module tt_um_adityarprasad_fft (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  
+    adaptive_fft_butterfly dut (
+        .clk    (clk),
+        .nreset (rst_n),
+
+        .ui_in  (ui_in),
+        .uo_out (uo_out),
+
+        .uio_in (uio_in),
+        .uio_out(uio_out),
+        .uio_oe (uio_oe)
+    );
 
 endmodule
+
+`default_nettype wire
